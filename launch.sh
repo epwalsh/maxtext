360726a74e6b558d063160bc9b510deb51099853
epwalsh <petew@allenai.org>
2025-07-15 15:40:10 -0700

    add launch script

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gantry run \
+    --show-logs \
+    --yes \
+    --workspace=ai2/OLMo-pretraining-stability \
+    --env-secret='GOOGLE_CREDENTIALS=GOOGLE_CREDENTIALS' \
+    --beaker-image=petew/olmax \
+    --system-python \
+    --gpu-type=b200 \
+    --gpus=8 -- \
+    ./end_to_end/gpu/mixtral/test_8x7b.sh
