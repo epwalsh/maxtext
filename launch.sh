ed8eb8e8ed8a6bb26893d3f00cbc41ac6ab383cc
epwalsh <petew@allenai.org>
2025-08-26 12:04:12 -0700

    set beaker token secret

--- launch.sh
+++ launch.sh
@@ -22,6 +22,7 @@ gantry run \
     --group=petew/B200_benchmarks_mixtral \
     --priority=high \
     --env-secret='GOOGLE_CREDENTIALS=GOOGLE_CREDENTIALS' \
+    --env-secret='BEAKER_TOKEN=PETEW_BEAKER_TOKEN' \
     --beaker-image=petew/olmax \
     --system-python \
     --gpu-type=b200 \
