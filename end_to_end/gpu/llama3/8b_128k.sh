cdbcc80aaaf29d9104f12438c164b3dea05b26b1
epwalsh <petew@allenai.org>
2025-08-31 15:14:00 -0700

    try this

--- end_to_end/gpu/llama3/8b_128k.sh
+++ end_to_end/gpu/llama3/8b_128k.sh
@@ -53,8 +53,8 @@ python3 -m MaxText.train MaxText/configs/base.yml \
     attention=cudnn_flash_te \
     dtype=bfloat16 \
     enable_checkpointing=false \
-    ici_fsdp_parallelism=-1 \
-    ici_context_parallelism=8 \
+    ici_fsdp_parallelism=4 \
+    ici_context_parallelism=4 \
     max_target_length=131072 \
     per_device_batch_size=1 \
     reuse_example_batch=1 \
