c35916ab992e4f5b89cbca08dfad13778d56544f
epwalsh <petew@allenai.org>
2025-09-08 16:54:27 -0700

    make patching script more robust

--- beaker/patch_fused_attention.sh
+++ beaker/patch_fused_attention.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-echo "Patching fused ring attention..."
+transformer_engine_location=$(python -c 'import transformer_engine; print(transformer_engine.__file__)')
+transformer_engine_location=$(dirname "$transformer_engine_location")
+echo "Patching fused ring attention to ${transformer_engine_location}..."
+
 wget https://gist.githubusercontent.com/epwalsh/8fbde5374638b62f49743a219831dc7c/raw/4c3dabe6d6ea51534fdc20ad6debaf1347fd7961/patched_attention.py
-mv patched_attention.py /usr/local/lib/python3.12/dist-packages/transformer_engine/jax/cpp_extensions/attention.py
+mv patched_attention.py "${transformer_engine_location}/jax/cpp_extensions/attention.py"
+
 wget https://gist.githubusercontent.com/epwalsh/94d6a0d506dcae906419df89b0e53ab2/raw/c517f112e2bb46f5ff306bc0aa7c3f466076d551/transformer.py
-mv transformer.py /usr/local/lib/python3.12/dist-packages/transformer_engine/jax/flax/transformer.py
+mv transformer.py "${transformer_engine_location}/jax/flax/transformer.py"
