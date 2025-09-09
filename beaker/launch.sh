636c5474567153c2d556b5018cb6c8e7ed4623c3
epwalsh <petew@allenai.org>
2025-09-08 18:00:52 -0700

    revert

--- beaker/launch.sh
+++ beaker/launch.sh
@@ -23,25 +23,6 @@ group_name="${name//-/_}"
 # - 01K3W08TB7WWCCC2CJEZA1Z98V (petew/maxtext from newer nvcr base image)
 # - 01JWSG3DFY30JECZQMPP3P0WXW (petew/olmax)
 
-gantry run \
-    --show-logs \
-    --yes \
-    --name="${name}-$(date +%Y%m%d-%H%M%S)" \
-    --description="MaxText ${name}" \
-    --group=petew/B200_benchmarks \
-    --group="petew/B200_benchmarks_${group_name}" \
-    --priority=urgent \
-    --task-timeout=120m \
-    --slack-webhook-url="$SLACK_WEBHOOK_URL" \
-    --env-secret='GOOGLE_CREDENTIALS=GOOGLE_CREDENTIALS' \
-    --env-secret='BEAKER_TOKEN' \
-    --beaker-image=petew/olmax-25.08 \
-    --system-python \
-    --post-setup=beaker/patch_fused_attention.sh \
-    --gpu-type=b200 \
-    --gpus=2 -- \
-    python beaker/fused_ring_attention_test.py
-
 # gantry run \
 #     --show-logs \
 #     --yes \
@@ -57,15 +38,34 @@ gantry run \
 #     --beaker-image=petew/olmax-25.08 \
 #     --system-python \
 #     --post-setup=beaker/patch_fused_attention.sh \
-#     --replicas=2 \
-#     --leader-selection \
-#     --host-networking \
-#     --propagate-failure \
-#     --propagate-preemption \
-#     --synchronized-start-timeout='5m' \
 #     --gpu-type=b200 \
-#     --gpus=8 -- \
-#     "./end_to_end/gpu/${script}"
+#     --gpus=2 -- \
+#     python beaker/fused_ring_attention_test.py
+
+gantry run \
+    --show-logs \
+    --yes \
+    --name="${name}-$(date +%Y%m%d-%H%M%S)" \
+    --description="MaxText ${name}" \
+    --group=petew/B200_benchmarks \
+    --group="petew/B200_benchmarks_${group_name}" \
+    --priority=urgent \
+    --task-timeout=120m \
+    --slack-webhook-url="$SLACK_WEBHOOK_URL" \
+    --env-secret='GOOGLE_CREDENTIALS=GOOGLE_CREDENTIALS' \
+    --env-secret='BEAKER_TOKEN' \
+    --beaker-image=petew/olmax-25.08 \
+    --system-python \
+    --post-setup=beaker/patch_fused_attention.sh \
+    --replicas=2 \
+    --leader-selection \
+    --host-networking \
+    --propagate-failure \
+    --propagate-preemption \
+    --synchronized-start-timeout='5m' \
+    --gpu-type=b200 \
+    --gpus=8 -- \
+    "./end_to_end/gpu/${script}"
 
     # --replicas=2 \
     # --leader-selection \
