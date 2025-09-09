7aceb6c91d02710eb2f3363880d3301bfeaa33d1
epwalsh <petew@allenai.org>
2025-09-09 09:06:54 -0700

    fix

--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -304,6 +304,7 @@ def validate_model_name(s: str) -> bool:
       "llama2-7b",
       "llama2-13b",
       "llama2-70b",
+      "llama3-1b",
       "llama3-8b",
       "llama3-70b",
       "llama3.1-8b",
