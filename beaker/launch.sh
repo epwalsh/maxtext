80a9accd8ce4745b2d8603a1532bac4aefd35d56
epwalsh <petew@allenai.org>
2025-09-10 11:08:50 -0700

    try this

--- beaker/launch.sh
+++ beaker/launch.sh
@@ -2,13 +2,14 @@
 
 set -ex
 
-script="${1:-mixtral/8x7b_8k.sh}"
+script="${1:-beaker/llama3_8b_8k.sh}"
 # Valid script names are:
 # - llama3/8b_8k.sh
 # - llama3/70b_8k.sh
 # - llama3/8b_128k.sh
 # - mixtral/8x7b_8k.sh
 
+name=$(basename "$script")
 # Remove file extension for naming.
 name="${script%.*}"
 # Replace slashes in path with dashes.
@@ -17,30 +18,6 @@ name="${name//\//-}"
 name="${name//_/-}"
 # Keep group name with underscores.
 group_name="${name//-/_}"
-
-# Image options:
-# - 01K3W6CM744M98H94HZGNWDY84 (petew/maxtext)
-# - 01K3W08TB7WWCCC2CJEZA1Z98V (petew/maxtext from newer nvcr base image)
-# - 01JWSG3DFY30JECZQMPP3P0WXW (petew/olmax)
-
-# gantry run \
-#     --show-logs \
-#     --yes \
-#     --name="${name}-$(date +%Y%m%d-%H%M%S)" \
-#     --description="MaxText ${name}" \
-#     --group=petew/B200_benchmarks \
-#     --group="petew/B200_benchmarks_${group_name}" \
-#     --priority=urgent \
-#     --task-timeout=120m \
-#     --slack-webhook-url="$SLACK_WEBHOOK_URL" \
-#     --env-secret='GOOGLE_CREDENTIALS=GOOGLE_CREDENTIALS' \
-#     --env-secret='BEAKER_TOKEN' \
-#     --beaker-image=petew/olmax-25.08 \
-#     --system-python \
-#     --post-setup=beaker/patch_fused_attention.sh \
-#     --gpu-type=b200 \
-#     --gpus=2 -- \
-#     python beaker/fused_ring_attention_test.py
 
 gantry run \
     --show-logs \
@@ -54,7 +31,7 @@ gantry run \
     --slack-webhook-url="$SLACK_WEBHOOK_URL" \
     --env-secret='GOOGLE_CREDENTIALS=GOOGLE_CREDENTIALS' \
     --env-secret='BEAKER_TOKEN' \
-    --beaker-image=petew/olmax-25.08 \
+    --beaker-image=petew/maxtext \
     --system-python \
     --post-setup=beaker/patch_fused_attention.sh \
     --replicas=2 \
@@ -76,3 +53,4 @@ gantry run \
     #
     # --beaker-image=petew/maxtext \
     # --install=beaker/install.sh \
+    # --beaker-image=petew/olmax-25.08 \
